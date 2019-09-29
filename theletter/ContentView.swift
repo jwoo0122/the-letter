0541b76e4cf6c10e6c9e073c6b4fbba8b4fd2b11
Jinwoo Jeong <thinkratomos@gmail.com>
2019-09-29 22:44:26 +0900

    Initial Commit

---
+++ theletter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  theletter
+//
+//  Created by 정진우 on 29/09/2019.
+//  Copyright © 2019 Jinwoo Jeong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
